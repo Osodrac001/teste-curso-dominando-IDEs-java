11494db3564b040d38f466828ee23a45d9e8b1d1
Osodrac <alcsantosfn@gmail.com>
2022-01-22 20:47:56 -0300

    first-commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package com.dio;
+
+import com.dio.modulo.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Gato gato = new Gato();
+		
+		/*int a = 3;
+		int b = 5;		
+		System.out.println("Hello World " + (a+b));*/
+	}
+
+}
